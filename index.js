33b7d0b13afafe7c1bd9fa22672cf1372d96bdaf
vinicius-godoy <v.godoyrodrigues@gmail.com>
2022-08-23 22:23:02 -0300

    Finish Factorial Comparison

--- index.js
+++ index.js
@@ -1,6 +1,34 @@
-// Import stylesheets
 import './style.css';
 
-// Write Javascript code!
-const appDiv = document.getElementById('app');
-appDiv.innerHTML = `<h1>My app</h1>`;
+// Functional Programming Example
+const getNumber = (inputId) => {
+  const value = document.getElementById(inputId).value;
+  return Math.abs(parseInt(value));
+};
+
+const getFunctionalFactorial = (n) => {
+  if (n <= 1) return 1;
+  else return n * getFunctionalFactorial(n - 1);
+};
+
+const displaySum = (value) => {
+  document.getElementById('func-sum-result').textContent = value;
+};
+
+document.getElementById('func-btn').onclick = () => {
+  const res = getFunctionalFactorial(getNumber('input-n'));
+  displaySum(res);
+};
+
+// Procedural Programming Example
+document.getElementById('oo-btn').onclick = () => {
+  const value = document.getElementById('input-n').value;
+  const n = Math.abs(parseInt(value));
+  let res = 1;
+
+  for (let i = 1; i <= n; i++) {
+    res = res * i;
+  }
+
+  document.getElementById('oo-sum-result').textContent = res;
+};
